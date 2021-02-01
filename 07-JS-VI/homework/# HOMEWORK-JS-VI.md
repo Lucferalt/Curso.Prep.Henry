88e42b8e16deaaa45e0fb2421adb397492e26b84
casa@DESKTOP-EE9NFM7 <lucferalt@gmail.com>
2021-02-01 06:35:50 -0300

    CP commiteado por lucferalt a las 1612172149550

---
+++ 07-JS-VI/homework/# HOMEWORK-JS-VI.md
@@ -0,0 +1,4 @@
+# HOMEWORK JS VI
+
+* las funciones "CallBacks" se le llama cuando tenemos una función y dentro del argumento 
+de esa función tenemos el nombre que nos lleva a otra función. 
--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -2,23 +2,33 @@
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
+  cb(invocarCallback);
 }
 
 function sumarArray(numeros, cb) {
   // Suma todos los números enteros (int/integers) de un array ("numeros")
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
+  var sumarArray = numeros.reduce(function(a, b){
+    return a + b
+  });  
+    cb(sumarArray);  
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
+  array.forEach(cb);
 }
 
 function map(array, cb) {
   // Crea un nuevo array
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
+  newArray = [];
+  array.forEach(cb);
+  var newArray = array.map(cb);
+  return newArray;
 }
 
 // No modificar nada debajo de esta línea
